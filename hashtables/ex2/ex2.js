86090f4c17295dd00eddffa8139dba6659085c14
Alex <alexandriaduell@gmail.com>
2023-03-21 19:31:35 -0400

    ex2 complete

--- hashtables/ex2/ex2.js
+++ hashtables/ex2/ex2.js
@@ -6,66 +6,37 @@ class Ticket {
 }
 
 const reconstruct_trip = (tickets, length) => {
-    // console.log(`Tickets:`, tickets)
-
     let hash_tickets = {}
     let route = []
-    let xTickets = {}
-    
+
     tickets.forEach(ticket => {
         hash_tickets[ticket.source] = ticket.destination
     })
 
-    console.log(`Hash Table:`, hash_tickets)
+    for(let i = 0; i < length; i++){
+        for(source in hash_tickets){
+            dest = hash_tickets[source]
+            let index = route.findIndex(item => {return item === source})
 
-    // for(let source in hash_tickets){
-    //     let destination = hash_tickets[source]
+            if(index > -1){
+                route.splice(index + 1, 0, dest)
+                delete hash_tickets[source]
+            }
+            if(route.length === 0){
+                route.push(source, dest)
+                delete hash_tickets[source]
+            }
+            if(source === `NONE`){
+                route.unshift(dest)
+                delete hash_tickets[source]
+            }
+            if(dest === `NONE` && !route.includes(`NONE`)){
+                route.push(dest)
+            }
+        }
+    }
 
-    //     if(route.length === 0){
-    //         route.push(source, destination)
-    //     }
-    //     else if(source === `NONE`){
-    //         route.unshift(destination)
-    //     }
-    //     else if(destination === `NONE` && hash_tickets[hash_tickets.length-1] === source){
-    //         route.push(destination)
-    //     }
-    //     else if(route[0] === destination){
-    //         route.unshift(source)
-    //     }
-    //     else if(route[route.length-1] === source){
-    //         route.push(destination)
-    //     }
-    //     else{
-    //         xTickets[source] = destination
-    //     }
-        // if(xTickets.length !== 0){
-        //     console.log(`HERE`)
-        //     // for(let xSource in xTickets){
-        //     //     let xDest = xTickets[xSource]
-        
-        //     //     if(xSource === `NONE`){
-        //     //         route.unshift(xDest)
-        //     //     }
-        //     //     else if(xDest === `NONE`){
-        //     //         route.push(xSource, xDest)
-        //     //     }
-        //     //     // else if(route[route.length-1] === `NONE` && route[0] !== xDest){
-        //     //     //     route.unshift(xSource, xDest)
-        //     //     // } 
-        //     //     else if(route[0] === xDest){
-        //     //         route.unshift(xSource)
-        //     //     }
-        //     //     else if(route[route.length-1] === xSource && xSource !== `NONE`){
-        //     //         route.push(xDest)
-        //     //     }
-        //     // }
-        // }
-    // }
-    // console.log(`xtra Tickets:`, xTickets)
-
-
-    console.log(`Route:`, route)
+    route = [...new Set(route)]
     return route
 }
 
