0b769eefbfcd5002369e18b0ecbcfef0081931a0
Alex <alexandriaduell@gmail.com>
2023-03-21 14:23:00 -0400

    ex4 complete

--- hashtables/ex4/ex4.js
+++ hashtables/ex4/ex4.js
@@ -1,5 +1,34 @@
 const has_negatives = a => {
-    //your code here
+    let hash_table = {}
+
+    a.forEach(num => {
+        if(num > 0){
+            if(hash_table[num] === 0){
+                hash_table[num]++
+            }else {
+                hash_table[num] = 0
+            }
+        } 
+        else if(num < 0){
+            num = -num
+
+            if(hash_table[num] === 0){
+                hash_table[num]++
+            }else {
+                hash_table[num] = 0 
+            }
+        }
+    })
+
+    let doubles = []
+
+    for(let num in hash_table){
+        if(hash_table[num] > 0){
+            doubles.push(parseInt(num))
+        }
+    }
+
+    return doubles
 }
 
 module.exports = {has_negatives}
--- hashtables/ex4/ex4.test.js
+++ hashtables/ex4/ex4.test.js
@@ -4,6 +4,6 @@ describe('has_negatives', ()=>{
     it('[1] small', ()=>{
         expect(has_negatives([1, 2, 3])).toEqual([])
         expect(has_negatives([1,2,3,-4])).toEqual([])
-        expect((has_negatives([-1,-2,1,2,3,4,-4])).sort()).toEqual([1, 2, 4])
+        expect((has_negatives([-1,-2,1,2,3,4,-4]))).toEqual([1, 2, 4])
     })
 })
